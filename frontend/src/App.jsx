03fcfa4a55f1074f1dc3c0244cd0e4b0dd1fdfa7
Nathan Fedzen <nfedzen@hotmail.com>
2025-05-09 22:07:19 -0600

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='container bg-red-500 mx-auto text-2xl'>
+        <h2 className='text-8xl text-amber-200'>Hello World</h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
